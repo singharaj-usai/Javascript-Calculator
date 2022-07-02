bc130499f6c534bb26063d99f983e7dc1684412a
Singharaj Usai <djjd22938@gmail.com>
2022-07-02 12:36:48 -0400

    finished?

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -23,4 +23,55 @@ function load() {
                 case '=':
                   // Last char of string
                   var lastChar = input[input.length - 1];
+                                    // Replace x to *, + to / which could be calculated in equal
+                                    input = input.replace(/x/g, '*').replace(/÷/g, '/');
+
+                                    // Checking the last character of the input.
+                                    // If it's an operator or a decimal, remove it
+                                    // /.$/ means last char in regex
+                                    if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+                                        input = input.replace(/.$/, '');
+                
+                                    if(input) {
+                                        // If the argument is an expression, equal() evaluates the expression.
+                                        // If the argument is one or more JavaScript statements, equal() executes the statements.
+                                        inputScreen.innerHTML = equal(input);
+                                    }
+                                    decimalAdded = false;
+                                    break;
+                                case '.':
+                                    if(!decimalAdded) {
+                                        inputScreen.innerHTML += btnValue;
+                                        decimalAdded = true;
+                                    }
+                                    break;
+                                case '÷':
+                                case 'x':
+                                case '-':
+                                case '+':
+                                    // Last char of string
+                                    var lastChar = input[input.length - 1];
+                
+                                    // Only add operator if input is not empty and there is no operator at the last
+                                    if(input != '' && operators.indexOf(lastChar) == -1)
+                                        inputScreen.innerHTML += btnValue;
+                
+                                    // Allows minus if the string is empty. The first number could be under zero
+                                    else if(input == '' && btnValue == '-')
+                                        inputScreen.innerHTML += btnValue;
+                
+                                    // Allows to represent the last operation
+                                    if(operators.indexOf(lastChar) > -1 && input.length > 1) {
+                                        inputScreen.innerHTML = input.replace(/.$/, btnValue);
+                                    }
+                                    decimalAdded = false;
+                                    break;
+                                default:
+                                    inputScreen.innerHTML += btnValue;
+                                    decimalAdded = false;
+                                    break;
+                            }
+                        });
+                    }
+                }
                 
