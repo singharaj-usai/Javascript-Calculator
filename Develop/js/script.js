d8dad58cd20962ecd8d437cbd8282c95e4329f45
Singharaj Usai <djjd22938@gmail.com>
2022-07-02 12:30:42 -0400

    javascript

---
+++ Develop/js/script.js
@@ -0,0 +1,8 @@
+function load() {
+
+    var btns = document.querySelectorAll('#calculator span');
+    var operators = ['+', '-', 'x', '÷'];
+    var inputScreen = document.querySelector('#screen');
+    var btnValue;
+    var input;
+}
